1add5df26bf2578a1e62f39c444a58113b7e79b1
Evan Raines <eraines92@gmail.com>
2020-02-25 10:06:21 -0500

    Update spark_udfs.scala

--- functions/spark_udfs.scala
+++ functions/spark_udfs.scala
@@ -13,7 +13,7 @@ def returnKeyVal(maps, key):
                 val += int(m[k])
     return val
 */
-def returnKeyVal[T](x: List[Map[T, Int]], k: T): Int = {
+def returnKeyVal[T](x: Array[Map[T, Int]], k: T): Int = {
     x.flatten.filter(_._1 == k).map(_._2).sum
 }
 
@@ -22,7 +22,7 @@ def returnKeySet(maps):
     """this returns the keys for each map"""
     return list(set([key for m in maps for key in m if key != '']))
 */
-def returnKeySet[T](x: List[Map[T, Any]]): List[T] = {
+def returnKeySet[T](x: Array[Map[T, Any]]): Array[T] = {
     x.flatMap(x => x.map(_._1)).distinct
 }
 
@@ -36,7 +36,7 @@ combineMap = udf(lambda maps: dict(zip(
     )
 */
 
-def reduceMaps(row: Seq[Map[String, String]]): Map[String, String] = {
+def reduceMaps(row: Array[Map[String, String]]): Map[String, String] = {
     row.flatten.groupBy(_._1).map(x => (x._1, x._2.map(_._2.toInt).sum.toString))
 }
 
@@ -53,4 +53,15 @@ def reduceMapConcatString(row: Map[String, String], c: String): String = {
     val s = row.toSeq.sortWith(_._2<=_._2).map(_._1)
     s.mkString(c)
 }
+
+/* bunch of udfs expressed as lambdas in python
+modalUdf = udf(lambda map: sum([int(float(map[x])) if "modal" in x else 0 for x in map]), IntegerType())                        # loop through map to find keyword and then sum list
+offeredUdf = udf(lambda map: sum([int(float(map[x])) if "subscriptions_offered" in x else 0 for x in map]), IntegerType())        # loop through map to find keyword and then sum list
+selectedUdf = udf(lambda map: sum([int(float(map[x])) if "subscription_selected" in x else 0 for x in map]), IntegerType())        # loop through map to find keyword and then sum list
+conversionUdf = udf(lambda map: sum([int(float(map[x])) if "conversion" in x else 0 for x in map]), IntegerType())                   # loop through map to find keyword and then sum list
+lowerUdf = udf(lambda arr: [x.lower() for x in arr], ArrayType(StringType()))
+*/
+
+def sumElementsUdf(m: Map[], ele: String): Int = m.filter(_._1.contains(ele)).map(_._2.toInt).sum 
+def lowerUdf(a: Array[String]): Array[String] = a.map(_.toLower)
 }
